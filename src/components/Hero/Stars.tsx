476f86867bb99dfb170c695cba0d0cd81a7e4524
Nico Tukiainen <nico.tukiainen@gmail.com>
2026-07-23 23:44:35 +0300

    Scatter more shooting stars across the sky

--- src/components/Hero/Stars.tsx
+++ src/components/Hero/Stars.tsx
@@ -7,6 +7,10 @@ function Stars() {
         <div className="startail-r"></div>
         <div className="startail-l"></div>
         <div className="startail-m"></div>
+        <div className="startail-x startail-x1"></div>
+        <div className="startail-x startail-x2"></div>
+        <div className="startail-x startail-x3"></div>
+        <div className="startail-x startail-x4"></div>
       </div>
       <div id="stars"></div>
     </>
